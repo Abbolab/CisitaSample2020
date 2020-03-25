717bbe64497cd167e719e6755da7417a0c70f53c
Francesco Abbo <francesco.abbo@xonne.it>
2020-03-25 17:57:27 +0100

    Creazione del modello utente

---
+++ app/src/main/java/it/xonne/cisitasample2020/models/User.java
@@ -0,0 +1,14 @@
+package it.xonne.cisitasample2020.models;
+
+import java.util.Date;
+
+public class User extends Object {
+
+    public String name = "";
+    public String surname = "";
+    public int age = 0;
+    public Date date;
+    public boolean visibile = true;
+
+
+}
